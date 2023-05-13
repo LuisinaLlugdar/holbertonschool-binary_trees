e12f3aff17fb91340aa499c4cace1050dd2539d7
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-05-12 17:57:52 -0700

    task 1 using task 0

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,22 +12,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	left_node = malloc(sizeof(binary_tree_t));
-	if (left_node == NULL)
-		return (NULL);
+	left_node = binary_tree_node(parent, value);
 
-	left_node->n = value;
 	if (parent->left == NULL)
 	{
 		parent->left = left_node;
-		left_node->parent = parent;
 	}
 	else
 	{
 		left_node->left = parent->left;
-		(parent->left)->parent = left_node;
 		parent->left = left_node;
-		left_node->parent = parent;
+		left_node->left->parent = left_node;
 	}
 	return (left_node);
 }
