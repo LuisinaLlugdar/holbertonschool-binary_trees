56a27b56c36ea40bd56010ca9a0050cfdd6345f9
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-05-16 10:22:46 -0700

    task 18 fixed betty, warning message didn`t appear

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_sibling - finds the sibling of a node
- * @node: pointer to the node to find the sibling
- * Return: pointer to the sibling node, or null if it doesn't exist
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to the node to find the uncle
+ * Return: pointer to the uncle node, or null if it doesn't exist
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
